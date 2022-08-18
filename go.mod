0b87ac346090f8d2d8b05322843b03be8fb6720c
Chunxu Zhang <chunxu.zhang@dolphindb.com>
2022-08-18 09:45:12 +0800

    feat: support cli arguments parsing

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/TangliziGit/dolphindb-jaeger
 
 go 1.18
 
-require github.com/apache/thrift v0.16.0
+require (
+	github.com/apache/thrift v0.16.0
+	github.com/jessevdk/go-flags v1.5.0
+)
+
+require golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
--- main.go
+++ main.go
@@ -3,26 +3,44 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
+
+	"github.com/jessevdk/go-flags"
 
 	"github.com/TangliziGit/dolphindb-jaeger/pkg/jaeger"
 	"github.com/TangliziGit/dolphindb-jaeger/pkg/spans"
 )
 
+var opts struct {
+	Host string `value-name:"host" short:"h" long:"host" default:"localhost" description:"The jaeger host"`
+	Port string `value-name:"port" short:"p" long:"port" default:"14268" description:"The jaeger thrift port"`
+	Args struct {
+		LogPath string `description:"The merged trace log file"`
+	} `positional-args:"true" required:"true"`
+}
+
 func main() {
-	if err := sendTrace(os.Args[1], os.Args[2]); err != nil {
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		if flags.WroteHelp(err) {
+			return
+		} else {
+			panic(err)
+		}
+	}
+
+	if err := sendTrace(opts.Args.LogPath, opts.Host, opts.Port); err != nil {
 		fmt.Println(err.Error())
 	}
 }
 
-func sendTrace(path string, host string) error {
+func sendTrace(path string, host string, port string) error {
 	spanMap, err := spans.BuildSpanMap(path)
 	if err != nil {
 		return err
 	}
 
 	batches := spans.BuildSpanBatch(spanMap)
-	uploader := jaeger.NewUploader("http://" + host + ":14268/api/traces")
+	uploader := jaeger.NewUploader(fmt.Sprintf("http://%s:%s/api/traces", host, port))
 	for _, batches := range batches {
 		err := uploader.Upload(context.Background(), batches)
 		if err != nil {
