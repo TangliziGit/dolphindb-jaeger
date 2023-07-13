f31c06bffbb8cb432694dc9653f59ebe66d45fb8
TangliziGit <tanglizimail@foxmail.com>
2023-07-13 18:23:41 +0800

    chore: update apt sources after add repository

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ if [ -z "$DOCKER" ]; then
       sudo apt-get install -y curl gnupg software-properties-common
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
       sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu "$VERSION_CODENAME" stable"
+      sudo apt-get update
       sudo apt-get install -y docker-ce
       sudo systemctl start docker
       ;;
